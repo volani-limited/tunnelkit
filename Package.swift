4d5ed350ce2fbae12e4fb9832c1c1ffe4af404b6
Oliver Bevan <oliver@volani.co.uk>
2025-03-27 17:03:46 +0000

    Remove wireguard targets/library.

--- Package.swift
+++ Package.swift
@@ -25,14 +25,6 @@ let package = Package(
             targets: ["TunnelKitOpenVPNAppExtension"]
         ),
         .library(
-            name: "TunnelKitWireGuard",
-            targets: ["TunnelKitWireGuard"]
-        ),
-        .library(
-            name: "TunnelKitWireGuardAppExtension",
-            targets: ["TunnelKitWireGuardAppExtension"]
-        ),
-        .library(
             name: "TunnelKitLZO",
             targets: ["TunnelKitLZO"]
         )
@@ -41,13 +33,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
-        //.package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "3.2.105"),
-//        .package(url: "https://git.zx2c4.com/wireguard-apple", .exact: Version("1.0.15-26")),
-//        .package(url: "https://github.com/passepartoutvpn/wireguard-apple", exact: Version("1.0.17")),
-        //.package(url: "https://github.com/passepartoutvpn/wireguard-apple", revision: "b79f0f150356d8200a64922ecf041dd020140aa0")
-            //.package(url: "https://github.com/volani-limited/wireguard-apple.git", branch: "master")
-//        .package(url: "https://github.com/passepartoutvpn/wireguard-apple", branch: "develop")
-//        .package(name: "WireGuardKit", path: "../wireguard-apple")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -111,32 +96,6 @@ let package = Package(
                 "TunnelKitOpenVPNProtocol"
             ]),
         //
-        .target(
-            name: "TunnelKitWireGuard",
-            dependencies: [
-                "TunnelKitWireGuardCore",
-                "TunnelKitWireGuardManager"
-            ]),
-        .target(
-            name: "TunnelKitWireGuardCore",
-            dependencies: [
-                "__TunnelKitUtils",
-                "TunnelKitCore",
-                .product(name: "WireGuardKit", package: "wireguard-apple"),
-                "SwiftyBeaver"
-            ]),
-        .target(
-            name: "TunnelKitWireGuardManager",
-            dependencies: [
-                "TunnelKitManager",
-                "TunnelKitWireGuardCore"
-            ]),
-        .target(
-            name: "TunnelKitWireGuardAppExtension",
-            dependencies: [
-                "TunnelKitWireGuardCore",
-                "TunnelKitWireGuardManager"
-            ]),
         .target(
             name: "TunnelKitLZO",
             dependencies: [],
